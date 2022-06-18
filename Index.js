b1a702efa41813abf9a585e1ed2a71241c36ebd3
DanielCarlos231 <89362488+DanielCarlos231@users.noreply.github.com>
2022-06-17 21:22:42 -0300

    Game dynamics complete

--- Index.js
+++ Index.js
@@ -122,6 +122,32 @@ function rectangularCollision({rectangle1,rectangle2}){
     );
 }
 
+function determineWinner({player, enemy, timerId}){   
+    clearTimeout(timerId);
+    document.querySelector('#displayText').style.display = 'flex'; 
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie';
+    } else if(player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins';
+    } else if(player.health < enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins';
+    }
+}
+
+let timer = 60;
+let timerId;
+function decreaseTimer() {
+    if (timer>0) {
+        timerId = setTimeout(decreaseTimer, 1000);
+        timer--;
+        document.querySelector('#timer').innerHTML = timer;
+    }
+    if (timer === 0) {
+        determineWinner({player, enemy, timerId});
+    }
+}
+decreaseTimer();
+
 function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black';
@@ -165,11 +191,16 @@ function animate() {
         enemy.isAttacking
         ) 
         {   
-            document.querySelector('#playerHealth').style.width = player.health +'%';
             player.health -= 20;                            
+            document.querySelector('#playerHealth').style.width = player.health +'%';
             enemy.isAttacking = false;
             console.log('Enemy is Attacking');
         }
+    
+    // End game by health    
+    if (player.health === 0 || enemy.health === 0 ){
+        determineWinner({player, enemy, timerId});   
+    }    
 }
 animate();
 
